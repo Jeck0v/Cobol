9ddd27b0c210938faaa5741b910379e1989e37ac
Arnaud F <149910271+Jeck0v@users.noreply.github.com>
2024-02-11 17:51:50 +0100

    Add files via upload

---
+++ HelloW.cbl
@@ -0,0 +1,10 @@
+       IDENTIFICATION DIVISION.
+       program-id. HelloW.
+       AUTHOR. Jecko.
+       procedure division.
+           DISPLAY "Bienvenue cher Coboliste".
+ 
+      *Ceci est un commentaire
+      *     
+      
+       
